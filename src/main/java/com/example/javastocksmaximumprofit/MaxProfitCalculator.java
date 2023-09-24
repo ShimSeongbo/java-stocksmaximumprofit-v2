d19f2c1ae02de04039d8c6ff75bcd98e7a4cd2b2
shimseongbo <seong265@icloud.com>
2023-09-24 09:57:39 +0900

    API Connect with WebClient

--- src/main/java/com/example/javastocksmaximumprofit/MaxProfitCalculator.java
+++ src/main/java/com/example/javastocksmaximumprofit/MaxProfitCalculator.java
@@ -1,5 +1,35 @@
 package com.example.javastocksmaximumprofit;
 
+import com.example.javastocksmaximumprofit.service.StockData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaxProfitCalculator {
 
+    public double maxProfitCalculator(List<StockData> stockDataList) {
+        List<Long> tempList = new ArrayList<>();
+        double maxProfit = 0;
+        double minPrice = 0;
+        double maxPrice = 0;
+        int size = stockDataList.size();
+
+        for (int i = 0; i < size - 1; i++) {
+            minPrice = stockDataList.get(i).getPrice();
+
+            for (int j = size - 1; i < j; j--) {
+                maxPrice = stockDataList.get(j).getPrice();
+                if (minPrice < maxPrice) {
+
+                    tempList.set(i, (long) (maxPrice - minPrice));
+
+                    if (maxProfit < tempList.get(i)) {
+                        maxProfit = tempList.get(i);
+                    }
+                }
+            }
+        }
+
+        return maxProfit;
+    }
 }
